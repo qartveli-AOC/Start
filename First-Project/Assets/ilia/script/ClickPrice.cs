b0d95b54fe1f1cca1797ba4e05573230e52044fc
qartveli-AOC <samqart@gmail.com>
2023-09-11 08:36:01 +0430

    Click cs
    Add Click Price cs

---
+++ First-Project/Assets/ilia/script/ClickPrice.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class ClickPrice : MonoBehaviour
+{
+    private int _score;
+    private int _count;
+    public TextMeshProUGUI Show_Count;
+    public TextMeshProUGUI Show_Price;
+    public TextMeshProUGUI Show_Score;
+    
+    
+    void Start()
+    {
+        _score = 0;
+        _count = 1;
+    }
+    
+
+   
+}
