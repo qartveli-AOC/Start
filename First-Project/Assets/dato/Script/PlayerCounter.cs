e07bb1a2ac47f1ea85f69799760488489b61aa08
dato-svg <139239441+dato-svg@users.noreply.github.com>
2023-09-13 15:46:12 +0400

    Fixed_Buttod
    Add Raycast add timer

--- First-Project/Assets/dato/Script/PlayerCounter.cs
+++ First-Project/Assets/dato/Script/PlayerCounter.cs
@@ -16,22 +16,35 @@ public class PlayerCounter : MonoBehaviour
     public TextMeshProUGUI Prefab_Text;
 
     public Button Garbige_Button;
+    public Image image;
+    public Sprite[] sprite;
 
    
+     
+     
+
 
     void Start()
     {       
-        Garbige_Button.onClick.AddListener(NumberInstantiante);
+        Garbige_Button.onClick.AddListener(SpawnPosition);
         Debug.Log("Inside");
         clickPrice = FindObjectOfType<ClickPrice>();
         
     }
     
-    void NumberInstantiante()
+  
+
+    void SpawnPosition()
     {
-        Prefab_Text.text = clickPrice.Count.ToString();
-        GameObject number = Instantiate(Number_Count_gameobject, transform.position, Quaternion.identity);
-        number.transform.SetParent(Transform_Canvas);
+        RaycastHit hit;
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray,out hit))
+        {
+            Prefab_Text.text = clickPrice.Count.ToString();
+            GameObject number = Instantiate(Number_Count_gameobject,hit.point, Quaternion.identity);
+            number.transform.SetParent(Transform_Canvas);
+        }
+        image.sprite = sprite[Random.Range(0,sprite.Length)];
 
     }
 
