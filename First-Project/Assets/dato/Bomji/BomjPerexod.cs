b215ffde294c74f5651d59a09369331fe5f958d2
dato-svg <139239441+dato-svg@users.noreply.github.com>
2023-09-24 14:36:44 +0400

    fixed homeless animation
    fixed homeless animation

---
+++ First-Project/Assets/dato/Bomji/BomjPerexod.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomjPerexod : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
